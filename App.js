735e95e6f973640fe8970549ee0b4e61b5c7c10b
Mish <mishuct26@gmail.com>
2019-09-28 22:09:27 +0200

    Specifies path location to the image logo

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ async function loadResourcesAsync() {
   await Promise.all([
     Asset.loadAsync([
       require('./assets/images/fluidity-image.png'),
-      require('./assets/images/fluidity-image.png'),
+      require('./assets/images/robot-prod.png'),
     ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
