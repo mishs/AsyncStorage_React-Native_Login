c14b1195440f54cd7e94dbfb79b1f68db02d52ce
Mish <mishuct26@gmail.com>
2019-09-29 21:36:42 +0200

    Align container and main body styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,39 @@
 import * as WebBrowser from 'expo-web-browser';
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   Text,
+  TextInput,
   Image,
   View,
+  // Alert,
+  // AsyncStorage
 } from 'react-native';
+// import $ from 'jquery'; 
+
+// import { Item } from 'native-base'
+// import { TextInput } from 'react-native-gesture-handler';
 
 
 class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+    // this.handleClick = this.handleClick.bind(this);
+
+  }
+
+
+  // handleClick = () => {
+  //   const { text } = this.state;
+  //   Alert.alert(text);
+  // }
+
   render(){
     return (
-      <View style={{height: 667, backgroundColor: "#F3F3F3"}}>
-        <View style={StyleSheet.mainbody}>
+      <View style={styles.container}>
+        <View style={styles.mainBody}>
           <View style={styles.welcomeContainer}>
             <Image style={styles.imgLogin}
               source={
@@ -21,23 +42,45 @@ class HomeScreen extends Component {
                   : require('../assets/images/fluidity-image.png')
               }
             />
-            <View>
-              <Text style={styles.labeluser}>
-                Username
-              </Text>
-            </View>
           </View>
+          <View>
+            {/* <TextInput 
+                placeholder="Type here to translate!"
+                style={{height: 40}}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+              /> */}
+             <Text style={styles.labelUser}>
+                Username
+             </Text>
+
+              {/* <Button
+          title="Press me"
+          onPress={this.handleClick}
+          />  */}
+            
+              <TextInput style={styles.inputUser} placeholder='username' />  
+          </View>
+
+
         </View>
       </View>
     )
   }
 }
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = {
-  mainbody: {
-    flex: 1
+  container: {
+    height: 667, 
+    backgroundColor: "#F3F3F3",
+  },
+  mainBody: {
+    flex: 1,
+    marginRight: 24,
+    marginLeft: 24,
+    backgroundColor: "#EDF1FA",
   },
   welcomeContainer: {
     alignItems: 'center',
@@ -51,8 +94,16 @@ const styles = {
     marginTop: 3,
     marginLeft: -10,
   },
-  labeluser: {
+  labelUser: {
     fontSize: 20,
-    color: "#414E93"
-  }
+    color: "#414E93",
+    marginBottom: 8,
+  },
+  inputUser: {
+    width: 280,
+    height: 45,
+    borderColor: 'red',
+    backgroundColor: "#414E93",
+    borderRadius: 8
+  },
 }
