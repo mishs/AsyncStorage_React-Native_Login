e35613a0ee83d13d6702800eefde51a4415fa034
Mish <mishuct26@gmail.com>
2019-09-30 13:09:09 +0200

    Assigns link value ot WelcomeScreen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,7 +4,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const config = Platform.select({
@@ -37,7 +37,7 @@ HomeStack.path = '';
 
 const LinksStack = createStackNavigator(
   {
-    Links: LinksScreen,
+    Links: WelcomeScreen,
   },
   config
 );
