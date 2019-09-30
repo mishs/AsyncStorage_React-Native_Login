54bd66b54669a4ef5638b11d8c7e0a8733cbb2e8
Mish <mishuct26@gmail.com>
2019-09-30 16:48:39 +0200

    Clears unwanted comments

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+  return (
+    <ScrollView style={styles.container}>
+    <Text>Welcome to Screen 3</Text>
+    <TouchableOpacity onPress={this.detailsValidate} style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Click to Get Back to Screen 2</Text>
+        </TouchableOpacity>
+  </ScrollView>
+);
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'app.json',
+  title: 'Screen 3',
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#7B1EA2',
+  },
+  buttonContainer: {
+    width: 280,
+    height: 45,
+    borderRadius: 5,
+    marginRight: 60,
+    backgroundColor: "#40A9F3", 
+    paddingVertical: 10
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
